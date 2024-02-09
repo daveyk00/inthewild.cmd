8df0afc60d54363697cb755f339d617783dabbb0
daveyk00 <114528910+daveyk00@users.noreply.github.com>
2024-02-09 15:57:12 +1000

    Create inthewild.cmd

---
+++ inthewild.cmd
@@ -0,0 +1,26 @@
+: --------------:
+: inthewild.cmd :
+: --------------:
+
+: Batch file to simplify running https://github.com/gmatuz/inthewilddb
+
+: Usage:
+: .\inthewild.cmd CVE-2021-30666
+: Not entering a CVE as an arguement will prompt for CVE
+
+: --------------:
+
+setlocal
+
+@echo off
+if "%~1"=="" goto :NoCVE
+set CVE=%~1
+
+goto :CVE
+:NoCVE
+set /p "CVE=Please Enter CVE in CVE-xxxx-yyyyy format: "
+
+:CVE
+docker run inthewild/inthewild reports %CVE%
+
+endlocal
